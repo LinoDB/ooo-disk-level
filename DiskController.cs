5747e67c9daa8ff0f0a6b23caa6df17bdc7a03df
LinoDB <linodelbianco@web.de>
2025-02-02 18:14:48 -0600

    Initial commit.

---
+++ DiskController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class DiskController : MonoBehaviour
+// TODO
+// - Can the disk always be on top of the player?
+// - Center the disk on the player (with z transformation).
+// - Move any other disk on the player back to their original spot.
+// - If already on the player, remove the z-shift for CheckDragActivation.
+{
+    public bool dragged = false;
+    public bool let_go = false;
+    private float offset;
+    private float cam_offset;
+    private Vector3 initial_position;
+    void Start() {
+        Transform center = this.gameObject.transform.GetChild(1);
+        this.offset = center.position.y - this.gameObject.transform.position.y;
+        this.cam_offset = this.gameObject.transform.position.z - Camera.main.transform.position.z;
+        this.initial_position = this.gameObject.transform.position;
+    }
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            // If the mouse gets pressed, check if the mouse is over the disk.
+            this.CheckDragActivation(Input.mousePosition);
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0)) {
+            // If the mouse gets released, let go of the disk.
+            this.let_go = true;
+        }
+    }
+    void LateUpdate()
+    {
+        if(this.let_go) {
+            // Place the disk back or on the disk player.
+            this.dragged = false;
+            this.let_go = false;
+            GameObject player = GameObject.Find("player");
+            if(this.CheckOnPlayer(player)) {
+                Vector3 player_center = player.transform.GetChild(1).position;
+                player_center.z -= .5f;
+                transform.position = player_center;
+            }
+            else {
+                transform.position = this.initial_position;
+            }
+        }
+        else if(this.dragged) {
+            // Place the disk where the mouse is.
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = this.cam_offset;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            mousePos.y -= this.offset;
+            transform.position = mousePos;
+        }
+    }
+    private void CheckDragActivation(Vector3 mousePos) {
+        // Check whether the mouse position is over the disk position.
+        mousePos.z = this.cam_offset;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Transform upperleft = this.gameObject.transform.GetChild(0);
+        Transform lowerright = this.gameObject.transform.GetChild(2);
+
+        if(
+            upperleft.position.x < mousePos.x &&
+            upperleft.position.y > mousePos.y &&
+            lowerright.position.x > mousePos.x &&
+            lowerright.position.y < mousePos.y
+        ) {
+            this.dragged = true;
+        }
+    }
+    private bool CheckOnPlayer(GameObject player) {
+        // Check whether the disk position is over the player.
+        Transform upperleft = player.transform.GetChild(0);
+        Transform lowerright = player.transform.GetChild(2);
+        if(
+            upperleft.position.x < this.gameObject.transform.position.x &&
+            upperleft.position.y > this.gameObject.transform.position.y &&
+            lowerright.position.x > this.gameObject.transform.position.x &&
+            lowerright.position.y < this.gameObject.transform.position.y
+        ) {
+            return true;
+        }
+        return false;
+    }
+}
