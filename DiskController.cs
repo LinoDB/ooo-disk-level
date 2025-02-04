82fcaf6c2f3e87cb844cf1ce9b2705099618ad23
LinoDB <linodelbianco@web.de>
2025-02-03 20:59:38 -0600

    Use game object references instead of a collider for the player because of overlapping colliders causing problems.

--- DiskController.cs
+++ DiskController.cs
@@ -10,7 +10,7 @@ public class DiskController : MonoBehaviour {
     private void Start() {
         this.cam_offset = this.gameObject.transform.position.z - Camera.main.transform.position.z;
         this.initial_position = this.gameObject.transform.position;
-        this.player = GameObject.Find("player-center");
+        this.player = GameObject.Find("player");
         this.controller = this.player.GetComponent<PlayerController>();
     }
     private void OnMouseDown() {
@@ -26,12 +26,11 @@ public class DiskController : MonoBehaviour {
     }
     private void OnMouseUp() {
         // Control object being dropped
-        if(controller.mouse_on) {
-            transform.position = player.transform.position;
+        if(this.CheckOnPlayer()) {
+            transform.position = player.transform.GetChild(1).position;
             this.in_player = true;
-            DiskController occupator = controller.occupied;
-            if(occupator) {
-                occupator.SendHome();
+            if(controller.occupied) {
+                controller.occupied.SendHome();
             }
             controller.occupied = this;
         }
@@ -48,6 +47,21 @@ public class DiskController : MonoBehaviour {
             mousePos.z = this.cam_offset;
             transform.position = Camera.main.ScreenToWorldPoint(mousePos);
         }
+    }
+
+    private bool CheckOnPlayer() {
+        // Check whether the disk position is over the player.
+        Transform upperleft = this.player.transform.GetChild(0);
+        Transform lowerright = this.player.transform.GetChild(2);
+        if(
+            upperleft.position.x < this.gameObject.transform.position.x &&
+            upperleft.position.y > this.gameObject.transform.position.y &&
+            lowerright.position.x > this.gameObject.transform.position.x &&
+            lowerright.position.y < this.gameObject.transform.position.y
+        ) {
+            return true;
+        }
+        return false;
     }
 
     public void SendHome() {
--- PlayerController.cs
+++ PlayerController.cs
@@ -2,12 +2,4 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public DiskController occupied = null;
-    public bool mouse_on = false;
-
-    private void OnMouseEnter() {
-        this.mouse_on = true;
-    }
-    private void OnMouseExit() {
-        this.mouse_on = false;
-    }
 }
