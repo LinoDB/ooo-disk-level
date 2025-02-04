bf2ed151e15002b34c8f293de9358e5dae8b0689
LinoDB <linodelbianco@web.de>
2025-02-03 19:41:58 -0600

    Use colliders to detect mouse events. Works well except the mouse events for disks in_player don't trigger.

--- DiskController.cs
+++ DiskController.cs
@@ -2,91 +2,56 @@ using UnityEngine;
 
 public class DiskController : MonoBehaviour {
     private bool dragged = false;
-    private bool let_go = false;
     private bool in_player = false;
-    // private float offset;
     private float cam_offset;
     private Vector3 initial_position;
-    void Start() {
-        Transform center = this.gameObject.transform.GetChild(1);
+    private GameObject player;
+    private PlayerController controller;
+    private void Start() {
         this.cam_offset = this.gameObject.transform.position.z - Camera.main.transform.position.z;
         this.initial_position = this.gameObject.transform.position;
+        this.player = GameObject.Find("player-center");
+        this.controller = this.player.GetComponent<PlayerController>();
     }
-    void Update() {
-        if(Input.GetKeyDown(KeyCode.Mouse0)) {
-            // If the mouse gets pressed, check if the mouse is over the disk.
-            this.CheckDragActivation(Input.mousePosition);
-            this.in_player = false;
+    private void OnMouseDown() {
+        // Control object being picked up
+        if(this.in_player) {
+            controller.occupied = null;
         }
-        else if(Input.GetKeyUp(KeyCode.Mouse0)) {
-            // If the mouse gets released, let go of the disk.
-            if(this.dragged) {
-                this.let_go = true;
-            }
-        }
+        this.in_player = false;
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = this.cam_offset;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        this.dragged = true;
     }
-    void LateUpdate()
-    {
-        if(this.let_go) {
-            // Place the disk back or on the disk player.
-            this.dragged = false;
-            this.let_go = false;
-            GameObject player = GameObject.Find("player");
-            if(this.CheckOnPlayer(player)) {
-                transform.position = player.transform.GetChild(1).position;
-                this.in_player = true;
-                PlayerController controller = player.GetComponent<PlayerController>();
-                DiskController occupator = controller.occupied;
-                if(occupator) {
-                    occupator.SendHome();
-                }
-                controller.occupied = this;
+    private void OnMouseUp() {
+        // Control object being dropped
+        if(controller.mouse_on) {
+            transform.position = player.transform.position;
+            this.in_player = true;
+            DiskController occupator = controller.occupied;
+            if(occupator) {
+                occupator.SendHome();
             }
-            else {
-                this.SendHome();
-            }
+            controller.occupied = this;
         }
-        else if(this.dragged) {
+        else {
+            this.SendHome();
+        }
+        this.dragged = false;
+    }
+
+    private void Update() {
+        if(this.dragged) {
             // Place the disk where the mouse is.
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = this.cam_offset;
             transform.position = Camera.main.ScreenToWorldPoint(mousePos);
         }
     }
-    private void CheckDragActivation(Vector3 mousePos) {
-        // Check whether the mouse position is over the disk position.
-        mousePos.z = this.cam_offset;
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        Transform upperleft = this.gameObject.transform.GetChild(0);
-        Transform lowerright = this.gameObject.transform.GetChild(2);
-        if(
-            upperleft.position.x < mousePos.x &&
-            upperleft.position.y > mousePos.y &&
-            lowerright.position.x > mousePos.x &&
-            lowerright.position.y < mousePos.y
-        ) {
-            this.dragged = true;
-            if(this.in_player) {
-                GameObject player = GameObject.Find("player");
-                player.GetComponent<PlayerController>().occupied = null;
-            }
-        }
-    }
-    private bool CheckOnPlayer(GameObject player) {
-        // Check whether the disk position is over the player.
-        Transform upperleft = player.transform.GetChild(0);
-        Transform lowerright = player.transform.GetChild(2);
-        if(
-            upperleft.position.x < this.gameObject.transform.position.x &&
-            upperleft.position.y > this.gameObject.transform.position.y &&
-            lowerright.position.x > this.gameObject.transform.position.x &&
-            lowerright.position.y < this.gameObject.transform.position.y
-        ) {
-            return true;
-        }
-        return false;
-    }
+
     public void SendHome() {
+        // Move the object to its initial position
         transform.position = this.initial_position;
         this.in_player = false;
     }
