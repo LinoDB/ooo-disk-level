76f58140c6c3c8faeaf930ae8ca98d94ddcabcc7
LinoDB <linodelbianco@web.de>
2025-02-08 21:58:14 -0600

    Cleanup.

--- DiskController.cs
+++ DiskController.cs
@@ -1,107 +1,117 @@
 using UnityEngine;
 
 public class DiskController : MonoBehaviour {
+    private const float transparency = .6f;
     private bool dragged = false;
     private bool in_player = false;
     private bool is_transparent = false;
     private float cam_offset;
-    private GameObject player;
     private int layer;
+    private PlayerController controller;
+    private Vector3 player_upperleft;
+    private Vector3 player_lowerright;
+    private Vector3 player_center;
+
     private void Start() {
-        this.cam_offset = this.gameObject.transform.position.z - Camera.main.transform.position.z;
-        this.layer = this.GetComponent<SpriteRenderer>().sortingOrder;
-        this.player = GameObject.Find("player");
+        // Initialize variables
+        cam_offset = transform.position.z - Camera.main.transform.position.z;
+        layer = GetComponent<SpriteRenderer>().sortingOrder;
+        GameObject player = GameObject.Find("player");
+        controller = player.GetComponent<PlayerController>();
+        player_upperleft = player.transform.Find("upperleft").position;
+        player_lowerright = player.transform.Find("lowerright").position;
+        player_center = player.transform.Find("center").position;
     }
+
     private void OnMouseDown() {
         // Control object being picked up
-        this.drag(true);
-        this.GetComponent<BoxCollider2D>().isTrigger = true;
-        if(this.in_player) {
-            this.player
-                .GetComponent<PlayerController>()
-                .occupied = null;
+        Drag(true);
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        if(in_player) {
+            controller.occupied = null;
         }
-        this.in_player = false;
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = this.cam_offset;
-        Rigidbody2D rigid_body = this.GetComponent<Rigidbody2D>();
+        in_player = false;
+
+        Rigidbody2D rigid_body = GetComponent<Rigidbody2D>();
         rigid_body.freezeRotation = true;
         rigid_body.isKinematic = true;
-        this.transform.eulerAngles = new Vector3(0, 0, 0);
-        this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = cam_offset;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
     }
+
     private void OnMouseUp() {
         // Control object being dropped
-        this.drag(false);
-        if(this.CheckOnPlayer()) {
-            PlayerController controller = this.player
-                .GetComponent<PlayerController>();
-            if(controller.occupied && controller.occupied.name != this.name) {
+        Drag(false);
+        if(CheckOnPlayer()) {
+            if(controller.occupied && controller.occupied.name != name) {
                 controller.occupied.SetFree();
             }
-            this.in_player = true;
+            in_player = true;
             controller.occupied = this;
-            this.transform.position = this.player.transform.Find("center").position;
+            transform.position = player_center;
         }
         else {
-            this.SetFree();
+            SetFree();
         }
     }
 
     private bool CheckOnPlayer() {
-        // Check whether the disk position is over the player.
-        Transform upperleft = this.player.transform.Find("upperleft");
-        Transform lowerright = this.player.transform.Find("lowerright");
+        // Check whether the disk position is over the player
         if(
-            upperleft.position.x < this.transform.position.x &&
-            upperleft.position.y > this.transform.position.y &&
-            lowerright.position.x > this.transform.position.x &&
-            lowerright.position.y < this.transform.position.y
+            player_upperleft.x < transform.position.x &&
+            player_upperleft.y > transform.position.y &&
+            player_lowerright.x > transform.position.x &&
+            player_lowerright.y < transform.position.y
         ) return true;
         return false;
     }
 
     private void Update() {
-        if(this.dragged) {
-            // Place the disk where the mouse is.
+        if(dragged) {
+            // Place the disk where the mouse is
             Vector3 mousePos = Input.mousePosition;
-            mousePos.z = this.cam_offset;
-            this.make_transparent(!this.CheckOnPlayer());
-            this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+            mousePos.z = cam_offset;
+            MakeTransparent(!CheckOnPlayer());
+            transform.position = Camera.main.ScreenToWorldPoint(mousePos);
         }
     }
 
-    private void drag(bool dragging) {
-        SpriteRenderer sprite = this.GetComponent<SpriteRenderer>();
+    private void Drag(bool dragging) {
+        // Toggle dragging state of the object
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
         if(dragging) {
-            this.make_transparent(!this.CheckOnPlayer());
-            sprite.sortingOrder = this.layer + 1;
+            MakeTransparent(!CheckOnPlayer());
+            sprite.sortingOrder = layer + 1;
         }
         else {
-            sprite.sortingOrder = this.layer;
+            sprite.sortingOrder = layer;
         }
-        this.dragged = dragging;
+        dragged = dragging;
     }
 
-    private void make_transparent(bool transp) {
-        if(this.is_transparent != transp) {
-            SpriteRenderer sprite = this.GetComponent<SpriteRenderer>();
+    private void MakeTransparent(bool transp) {
+        // Set transparency of the object
+        if(is_transparent != transp) {
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
             Color white = Color.white;
             if(transp) {
-                white.a = .6f;
+                white.a = transparency;
             }
             sprite.color = white;
-            this.is_transparent = transp;
+            is_transparent = transp;
         }
     }
 
     public void SetFree() {
-        // Move the object to its initial position
-        this.GetComponent<BoxCollider2D>().isTrigger = false;
-        Rigidbody2D rigid_body = this.GetComponent<Rigidbody2D>();
+        // Activate physics on the object
+        GetComponent<BoxCollider2D>().isTrigger = false;
+        Rigidbody2D rigid_body = GetComponent<Rigidbody2D>();
         rigid_body.freezeRotation = false;
         rigid_body.isKinematic = false;
-        this.make_transparent(false);
-        this.in_player = false;
+        MakeTransparent(false);
+        in_player = false;
     }
 }
