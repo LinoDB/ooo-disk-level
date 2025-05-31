ca49e3c8822fdb949fba833b17cd0a5bd474659d
LinoDB <linodelbianco@web.de>
2025-05-31 15:53:11 -0600

    Add pipe spawning logic.

--- Game_1.cs
+++ Game_1.cs
@@ -1,10 +1,79 @@
+using System.IO.Pipes;
+using System.Threading;
 using UnityEngine;
 
-public class Game_1 : GameController {
+/*
+    - Make hardcoded pipe positions instead relative to the display size
+    - Make a global speed value on Game_1 that all pipes update with
+*/
+
+public class Game_1 : GameController
+{
     public GameObject pipe;
+    public float speed;
+    private float pacer;
+    private float start_time;
+    private int spawn_rate_pointer;
+    private float[] spawn_rates = { 3f, 2f, 1f, .7f };
+    private float switch_spawn_rate = 30f;
+    private float additional_delay = 0f;
 
     public override void StartGame() {
-        Instantiate(pipe);
+        spawn_rate_pointer = 0;
+        start_time = Time.time;
+        pacer = start_time - spawn_rates[spawn_rate_pointer];
     }
 
+    public override void Update() {
+        if(spawn_rate_pointer < spawn_rates.Length - 1) {
+            if(Time.time > start_time + switch_spawn_rate) {
+                spawn_rate_pointer += 1;
+                start_time = Time.time;
+            }
+        }
+        if(
+            Time.time > pacer +
+            spawn_rates[spawn_rate_pointer] +
+            additional_delay
+        ) {
+            Vector3 pos = transform.position;
+            pos.x += 2.2f;
+            if(Random.Range(0, 3) == 1){
+                top_pipe(pos);
+            }
+            else {
+                bottom_pipe(pos);
+            }
+            pacer = Time.time;
+            additional_delay = Random.Range(
+                0, 0.5f * spawn_rates[spawn_rate_pointer]
+            );
+        }
+    }
+
+    private void bottom_pipe(Vector3 pos) {
+        Quaternion rot = new Quaternion(0, 0, 0, 1);
+        pos.y -= Random.Range(0.6f, 2f);
+        Instantiate(pipe, pos, rot, this.transform);
+    }
+
+    private void top_pipe(Vector3 pos) {
+        Quaternion rot = new Quaternion(0, 0, 180, 1);
+        pos.y += Random.Range(0.6f, 2f);
+        Instantiate(pipe, pos, rot, this.transform);
+    }
+
+    private void dual_pipe() {
+        
+    }
+
+    public void OnDisable() {
+        // destroy remaining pipes
+        for(int i = transform.childCount; i > 0; i--){
+            GameObject g_o = transform.GetChild(i - 1).gameObject;
+            if(g_o.name.ToLower().Contains("pipe")){
+                Destroy(g_o);
+            }
+        }
+    }
 }
--- Game_1_Pipe.cs
+++ Game_1_Pipe.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 
 public class Game_1_Pipe : MonoBehaviour {
-    private float speed = 5.0f;
+    private float speed = 1.0f;
+    private Transform top;
+
+    private void Start()
+    {
+        top = transform.GetChild(0);
+    }
 
     private void Update () {
         Vector3 pos = transform.position;
         pos.x -= Time.deltaTime * speed;
         transform.position = pos;
+        CheckAlive();
     }
 
-    private void OnTriggerEnter2D(Collider2D trigger) {
-        if(trigger.CompareTag("Death")) {
-            Destroy(gameObject);
-        }
+    private void CheckAlive() {
+        Ray ray = Camera.main.ScreenPointToRay(
+            Camera.main.WorldToScreenPoint(top.position)
+        );
+        if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, 2)) {
+            if(hit.collider.CompareTag("Death")) {
+			    Destroy(gameObject);
+            }
+		}
     }
 }
